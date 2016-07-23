b0cf20ca0205551348cc1e13a40be3b2c775d949
mohamedashrafothman <mohamedashrafothman@gmail.com>
2016-07-23 15:48:50 +0300

    add courses section

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
 
 	// slick carousel
-  var gadgetCarousel = $(".single-item, .multiple-items");
+  var gadgetCarousel = $(".single-item, .multiple-items, .responsive");
 
   gadgetCarousel.each(function() {
 	if ($(this).is(".single-item")) {
@@ -21,6 +21,43 @@ $(document).ready(function() {
 		    slidesToShow: 3,
 		    slidesToScroll: 2
 		});
+	}
+	else if($(this).is(".responsive")) {
+		$(this).slick({
+			  dots: false,
+			  infinite: false,
+			  speed: 300,
+			  slidesToShow: 4,
+			  slidesToScroll: 1,
+			  responsive: [
+			    {
+			      breakpoint: 1024,
+			      settings: {
+			        slidesToShow: 3,
+			        slidesToScroll: 3,
+			        infinite: true,
+			        dots: true
+			      }
+			    },
+			    {
+			      breakpoint: 600,
+			      settings: {
+			        slidesToShow: 2,
+			        slidesToScroll: 2
+			      }
+			    },
+			    {
+			      breakpoint: 480,
+			      settings: {
+			        slidesToShow: 1,
+			        slidesToScroll: 1
+			      }
+			    }
+			    // You can unslick at a given breakpoint now by adding:
+			    // settings: "unslick"
+			    // instead of a settings object
+			  ]
+			});
 	}
 	else {
 	  $(this).slick();
